e1e69d8b87e00c67c2b40a2afced7b5d51fa44d7
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-08-02 10:20:29 +0200

    Add e.preventDefault() for form

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const modalContainer = document.getElementById("modal-container");
 const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("consent-form");
 
 setTimeout(function () {
   modalContainer.style.display = "inline";
@@ -7,4 +8,9 @@ setTimeout(function () {
 
 modalCloseBtn.addEventListener("click", function () {
   modalContainer.style.display = "none";
+});
+
+consentForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("form submitted");
 });
