0e0b6541df6ededc402b224b3f66cea953951775
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2023-08-02 11:30:13 +0200

    Add messages to modal form

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const modalContainer = document.getElementById("modal-container");
 const modalCloseBtn = document.getElementById("modal-close-btn");
 const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
 
 setTimeout(function () {
   modalContainer.style.display = "inline";
@@ -12,5 +13,23 @@ modalCloseBtn.addEventListener("click", function () {
 
 consentForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("form submitted");
+  modalText.innerHTML = `<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="upload-text">
+        Uploading your data to the dark web...
+    </p>
+</div>`;
+  setTimeout(function () {
+    document.getElementById("upload-text").innerText = `Making the sale...`;
+  }, 1500);
+
+  setTimeout(function () {
+    document.getElementById(
+      "modal-content"
+    ).innerHTML = `<h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>`;
+  }, 3000);
 });
